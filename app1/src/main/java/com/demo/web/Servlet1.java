13dd1c09818ab8c38c49555ed7a6cafa83fbf1c0
sbtalk71 <sbtalk@yahoo.com>
2023-08-16 17:24:21 +0530

    updated

--- app1/src/main/java/com/demo/web/Servlet1.java
+++ app1/src/main/java/com/demo/web/Servlet1.java
@@ -13,6 +13,7 @@ public class Servlet1 extends GenericServlet{
 	@Override
 	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
 		
+		System.out.println(Thread.currentThread().getName());
 	resp.setContentType("text/html");
 	PrintWriter out =resp.getWriter();
 	out.write("<h1>Hello from Servlet</h1>");
